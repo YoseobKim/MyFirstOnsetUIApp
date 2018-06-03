5eaf9ed79e9c74df63911fcdee93f626eadff90d
Yoseob Kim <yo1226@gmail.com>
2018-06-03 09:18:42 +0000

    Image, dynamic Toolbar added

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -4,6 +4,13 @@ import {Page, Button, Toolbar, BackButton} from 'react-onsenui';
 import {notification} from 'onsenui';
 
 export default class Page2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+  }
+
   alertPopup() {
     notification.alert('This is an Onsen UI alert notification test.');
   }
@@ -12,23 +19,34 @@ export default class Page2 extends React.Component {
     this.props.navigator.popPage();
   }
 
+  goNext() {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  }
+
   renderToolbar() {
+    var title = 'Onsen UI ' + this.state.counter;
     return (
       <Toolbar>
-        <div className="left"><BackButton>Back</BackButton></div>
-        <div className='center'>Onsen UI</div>
+        <BackButton className='left'>Back</BackButton>
+        <div className='center'>{title}</div>
       </Toolbar>
     );
   }
 
   render() {
+    var imgStyle = {
+        width: '100%'
+    };
     return (
-      <Page renderToolbar={this.renderToolbar}>
+      <Page renderToolbar={this.renderToolbar.bind(this)}>
         <h3>My First OnsenUI Hybrid App</h3>
-         <img src="http://images.all-free-download.com/images/graphicthumb/two_young_cats_189918.jpg"/>
+        <img style={imgStyle} src="img/two_young_cats_189918.jpg"/>
         <div>Page2</div>
         <Button onClick={this.alertPopup}>Click Me!</Button>
         <Button onClick={this.goBack.bind(this)}>Go Back!</Button>
+        <Button onClick={this.goNext.bind(this)}>Go Next!</Button>
       </Page>
     );
   }
